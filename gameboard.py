d5285cb85eb9f2fb25e960d0faad693c5f5e97d6
Erik <erik@Knuts-MBP.lan>
2023-12-18 14:39:21 +0100

    Add basic print method to gameboard

--- gameboard.py
+++ gameboard.py
@@ -1,4 +1,15 @@
 class GameBoard:
 	def __init__(self, state=[[0 for i in range(3)] for i in range(3)], turn=0):
 		self.state = state
-		self. turn
+#		self.turn = turn
+	def print(self):
+		print(" ------- ")
+		currentline = "| "
+		for i in range(3):
+			for j in range(3):
+				currentline += str(self.state[i][j])
+				if j != 2: currentline += " "
+			currentline += " |"
+			print(currentline)
+			currentline = "| "
+		print(" ------- ")
